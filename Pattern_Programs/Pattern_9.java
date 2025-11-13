ab90961465e95c696728482a959185177234b097
pritisharma1511 <ps0130815@gmail.com>
2025-11-13 22:29:52 +0530

    done

---
+++ Pattern_Programs/Pattern_9.java
@@ -0,0 +1,20 @@
+public class Pattern_9{
+    public static void main(String[] args) {
+        int rows = 5;
+
+        // Outer loop (must decrement i)
+        for (int i = 1; i <= 5; i++) {
+    for (int space = i; space < 5; space++) {
+        System.out.print(" ");
+    }
+    for (int j = 1; j <= i; j++) {
+        System.out.print(j);
+    }
+    for (int j = i - 1; j >= 1; j--) {
+        System.out.print(j);
+    }
+    System.out.println();
+}
+
+    }
+}
