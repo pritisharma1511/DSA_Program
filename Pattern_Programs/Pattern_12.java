761b4eeeb1b11d95fa14f912177d5e8540a99c92
pritisharma1511 <ps0130815@gmail.com>
2025-11-13 22:43:19 +0530

    done all

---
+++ Pattern_Programs/Pattern_12.java
@@ -0,0 +1,14 @@
+public class Pattern_12 {
+    public static void main(String[] args) {
+        int rows = 5;
+        char ch = 'A';
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+            }
+            ch++; // move to next letter
+            System.out.println();
+        }
+    }
+}
